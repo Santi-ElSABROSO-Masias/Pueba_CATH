fb21d16327dbdfb4c0e710da9987756eb52208d1
Santi-ElSABROSO-Masias <101581953+Santi-ElSABROSO-Masias@users.noreply.github.com>
2026-03-29 06:23:51 -0500

    Refactor: move evaluaciones components to module

---
+++ src/modules/evaluaciones/index.ts
@@ -0,0 +1,7 @@
+export { EvaluacionesModule } from './components/EvaluacionesModule';
+export { ExamAccess } from './components/ExamAccess';
+export { ExamAnalytics } from './components/ExamAnalytics';
+export { ExamEmailShare } from './components/ExamEmailShare';
+export { ExamParticipantInfo } from './components/ExamParticipantInfo';
+export { ExamPublish } from './components/ExamPublish';
+export { QuestionBank } from './components/QuestionBank';
